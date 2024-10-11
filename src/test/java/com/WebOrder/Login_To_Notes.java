f526e874ddbfd0b3408bc9fbad9733b8beadc49a
PallaviJoshi25 <pallavijo254@gmail.com>
2024-10-11 04:42:03 +0000

    adding new script

---
+++ src/test/java/com/WebOrder/Login_To_Notes.java
@@ -0,0 +1,39 @@
+package com.WebOrder;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+public class Login_To_Notes {
+	WebDriver driver;
+	  @Test	(priority = 1)	//annotation (for executing without main function) with priority 
+	  public void login() throws InterruptedException {
+		  driver = new ChromeDriver();
+		  driver.get("http://secure.smartbearsoftware.com/samples/TestComplete11/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2fTestComplete11%2fWebOrders%2fDefault.aspx");
+		  driver.manage().window().maximize();
+		  
+		  driver.findElement(By.name("ctl00$MainContent$username")).sendKeys("Tester");
+		  driver.findElement(By.name("ctl00$MainContent$password")).sendKeys("test");
+		  driver.findElement(By.name("ctl00$MainContent$login_button")).click();
+		  driver.findElement(By.linkText("Logout")).isDisplayed(); 
+		  
+		  Thread.sleep(5000); 
+		  
+	  }
+	  
+	  @Test(priority = 2)	//setting priority for execution
+	  public void logout() throws InterruptedException {
+		  driver.findElement(By.linkText("Logout")).click(); 
+		  driver.findElement(By.name("ctl00$MainContent$login_button")).isDisplayed();
+		  Thread.sleep(5000); 
+	  }
+	  
+	  @AfterTest
+	  public void closebrowser() {
+		  driver.quit(); //close -> current browser
+			             //quit -> all the browsers
+	  }
+
+}
